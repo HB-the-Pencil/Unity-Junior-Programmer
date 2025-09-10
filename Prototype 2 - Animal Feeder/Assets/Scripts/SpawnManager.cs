72e128e454f5347776cf5d2ed249ffad8886c38a
jroque6 <henry.barcalow@gmail.com>
2025-09-10 11:44:33 -0400

    M2 - Animals spawn randomly

--- Prototype 2 - Animal Feeder/Assets/Scripts/SpawnManager.cs
+++ Prototype 2 - Animal Feeder/Assets/Scripts/SpawnManager.cs
@@ -6,23 +6,28 @@ public class SpawnManager : MonoBehaviour
     
     private float _spawnRangeX = 15;
     private float _spawnRangeZ = 20;
+
+    private float _startDelay = 2;
+    private float _spawnInterval = 1.5f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", _startDelay, _spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 animalPosition = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnRangeZ);
-            
-            Instantiate(animalPrefabs[animalIndex], animalPosition, 
-                animalPrefabs[animalIndex].transform.rotation);
-        }
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 animalPosition = new Vector3(
+            Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnRangeZ);
+        Instantiate(animalPrefabs[animalIndex], animalPosition, 
+            animalPrefabs[animalIndex].transform.rotation);
     }
 }
